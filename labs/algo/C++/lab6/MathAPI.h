0750d11b22ec1b6e459f6430d2aadaec39dc919d
DenisMasterHerobrine <27882185+DenisMasterHerobrine@users.noreply.github.com>
2022-10-21 22:24:54 +0500

    Added a note into header file.

--- labs/algo/C++/lab6/MathAPI.h
+++ labs/algo/C++/lab6/MathAPI.h
@@ -1,5 +1,15 @@
 #pragma once
 
+/*
+	MathAPI v0.0.1 - MathAPI Header - MathAPI.h;
+	A part of C++ Math module of a currently unnamed project to use in my C++/Java projects.
+	No source code available at the moment. Names of the methods are changed to fit the task requirements.
+
+	Compiled against Microsoft Visual Studio Community 2022 v17.3.3
+	Made by Denis Kalashnikov (DMHDev)
+	Licensed under MIT License.
+*/
+
 int NOD(int, int);
 
 int SOKR(int&, int&);
